a0370d186e87ff0e5553271d97045cdbfe0d25db
miangaly-rakotoarivelo <miangaly.rakotoarivelo@pulse.mg>
2026-02-03 15:29:14 +0300

    change logo and header layout

--- src/app/components/main-content/layout/pages/home/home.ts
+++ src/app/components/main-content/layout/pages/home/home.ts
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './home.scss',
 })
 export class Home {
-
+  cards = [
+    {
+      title: 'Women',
+      caption: 'New arrivals',
+      img: 'assets/images/woman.png',
+      alt: 'woman',
+    },
+    {
+      title: 'Men',
+      caption: 'Best sellers',
+      img: 'assets/images/man.png',
+      alt: 'man',
+    },
+    {
+      title: 'Kids',
+      caption: 'All sizes',
+      img: 'assets/images/kids.png',
+      alt: 'kid',
+    },
+    {
+      title: 'Accessories',
+      caption: 'Trending now',
+      img: 'assets/images/accessories.png',
+      alt: 'accessories',
+    },
+  ];
 }
