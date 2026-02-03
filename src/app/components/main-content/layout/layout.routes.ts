0382b13408b88555e22140c569e2987ce1344eb0
MiaRak <95743543+MiaRak@users.noreply.github.com>
2026-02-03 10:06:05 +0300

    Deploy layout (#4)

--- src/app/components/main-content/layout/layout.routes.ts
+++ src/app/components/main-content/layout/layout.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./layout').then((c) => c.Layout),
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then((c) => c.Home),
+      },
+    ],
+  },
+];
