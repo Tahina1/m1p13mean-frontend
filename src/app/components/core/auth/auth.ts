ea43e8630b1e2a9d927bee5d9a156990cfcc6ea0
MiaRak <miangalyrakotoarivelo2@gmail.com>
2026-02-05 20:16:21 +0300

    intégration auth start + dynamisation form register with deployed backend

--- src/app/components/core/auth/auth.ts
+++ src/app/components/core/auth/auth.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { AuthService } from '@/components/shared/services/auth';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+type Role = 'ADMIN' | 'SHOP' | 'CLIENT';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
 })
 export class Auth {
+  isLogin = true;
+  private fb = inject(FormBuilder);
+  form = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    email: [''],
+    password: [''],
+    roles: this.fb.control<Role[]>(['CLIENT']),
+  });
+  authService = inject(AuthService);
 
+  login() {
+    const { email, password } = this.form.value;
+
+    if (!email || !password) return;
+
+    this.authService.login({ email, password }).subscribe({
+      next: (res) => console.log('LOGIN:', res),
+      error: (err) => console.error('LOGIN ERROR:', err),
+    });
+  }
+
+  register() {
+    const { firstName, lastName, email, password, roles } = this.form.value;
+
+    this.authService
+      .register({
+        firstName: firstName ?? '',
+        lastName: lastName ?? '',
+        email: email ?? '',
+        password: password ?? '',
+        roles: roles ?? ['CLIENT'],
+      })
+      .subscribe({
+        next: (res) => console.log('REGISTER:', res),
+        error: (err) => console.error('REGISTER ERROR:', err),
+      });
+  }
 }
--- src/app/components/core/components/header/header.ts
+++ src/app/components/core/components/header/header.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AuthService } from '@/components/shared/services/auth';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,4 +8,6 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+  authService = inject(AuthService);
+}
