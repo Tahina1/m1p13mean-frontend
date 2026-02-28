4a6d0bb7fef576389cdc849ab291c23dad944325
MiaRak <miangalyrakotoarivelo2@gmail.com>
2026-02-28 21:59:42 +0300

    session storage

--- src/app/components/core/guards/shop-guard.ts
+++ src/app/components/core/guards/shop-guard.ts
@@ -1,20 +1,22 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
-export const shopGuard: CanActivateFn = (route, state) => {
+export const shopGuard: CanActivateFn = () => {
   const router = inject(Router);
 
-  const user = localStorage.getItem('user');
+  const user = JSON.parse(sessionStorage.getItem('user') || 'null');
+
   if (!user) {
-    router.navigate(['/home']);
+    router.navigate(['/']);
     return false;
   }
 
-  const parsed = JSON.parse(user);
-  const role = parsed.roles?.[0];
+  const role = user.roles?.[0];
 
-  if (role === 'SHOP') return true;
+  if (role !== 'SHOP') {
+    router.navigate(['/']);
+    return false;
+  }
 
-  router.navigate(['/home']);
-  return false;
+  return true;
 };
