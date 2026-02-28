c709b3f160eb7087423df484e408a502c78e46db
MiaRak <miangalyrakotoarivelo2@gmail.com>
2026-02-28 21:13:54 +0300

    admindashboard start

--- src/app/components/main-content/admin-layout/admin-layout.ts
+++ src/app/components/main-content/admin-layout/admin-layout.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Aside } from '@/components/core/components/aside/aside';
+import { AuthService } from '@/components/shared/services/auth';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [RouterOutlet, Aside],
   templateUrl: './admin-layout.html',
   styleUrl: './admin-layout.scss',
 })
-export class AdminLayout {
-
-}
+export class AdminLayout {}
