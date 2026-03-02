f31ff06945a382fc78984259bd9e1d26531ee44a
MiaRak <miangalyrakotoarivelo2@gmail.com>
2026-03-02 10:26:52 +0300

    products page shop owner

--- src/app/components/main-content/shop-layout/shop-layout.routes.ts
+++ src/app/components/main-content/shop-layout/shop-layout.routes.ts
@@ -5,15 +5,19 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./shop-layout').then((c) => c.ShopLayout),
     children: [
-      //   {
-      //     path: '',
-      //     redirectTo: 'home',
-      //     pathMatch: 'full',
-      //   },
-      //   {
-      //     path: 'home',
-      //     loadComponent: () => import('./pages/home/home').then((c) => c.Home),
-      //   },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then((c) => c.Dashboard),
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('./pages/product-list/product-list').then((c) => c.ProductList),
+      },
     ],
   },
 ];
--- src/app/components/main-content/shop-layout/shop-layout.ts
+++ src/app/components/main-content/shop-layout/shop-layout.ts
@@ -1,39 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
-import { ProductModal } from './product-modal/product-modal';
-import { AuthService } from '@/components/shared/services/auth';
-import { ShopService } from '@/components/shared/services/shop-service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shop-layout',
-  imports: [ProductModal],
+  imports: [RouterOutlet],
   templateUrl: './shop-layout.html',
   styleUrl: './shop-layout.scss',
 })
-export class ShopLayout {
-  authService = inject(AuthService);
-  shopService = inject(ShopService);
-  isModalOpen = false;
-
-  shop = signal<any>(null);
-
-  ngOnInit() {
-    const shopId = this.authService.shopId();
-
-    if (!shopId) {
-      console.error('No shop linked');
-      return;
-    }
-
-    this.shopService.getShopById(shopId).subscribe((res) => {
-      this.shop.set(res);
-    });
-  }
-
-  openModal() {
-    this.isModalOpen = true;
-  }
-
-  closeModal() {
-    this.isModalOpen = false;
-  }
-}
+export class ShopLayout {}
