fa1779ab267e4d14e90a1f4a05973f16d1010ed0
miangaly-rakotoarivelo <miangaly.rakotoarivelo@pulse.mg>
2026-03-04 11:47:12 +0300

    login default

--- src/app/components/core/auth/login/login.ts
+++ src/app/components/core/auth/login/login.ts
@@ -24,70 +24,19 @@ export class Login {
 
   login() {
     const data = this.form.getRawValue();
-    this.authService.loading.set(true);
 
     this.authService.login(data).subscribe({
       next: (res: any) => {
-        if (res.accessToken) {
-          localStorage.setItem('token', res.accessToken);
+        const role = this.authService.activeRole();
+
+        if (role === 'ADMIN') {
+          this.router.navigate(['/admin'], { replaceUrl: true });
+        } else if (role === 'SHOP') {
+          this.router.navigate(['/shop-dashboard'], { replaceUrl: true });
         } else {
-          console.error('NO TOKEN RECEIVED FROM BACKEND', res);
+          this.router.navigate(['/home'], { replaceUrl: true });
         }
-        localStorage.setItem('user', JSON.stringify(res.user));
-        this.authService.userSignal.set(res.user);
-        this.authService.setAuthPopup(false);
-
-        const roles = res.user.roles || [];
-
-        this.authService.shopId.set(res.user.shopId || null);
-
-        // Decide default active role
-        let defaultRole: 'ADMIN' | 'SHOP' | 'CLIENT' = 'CLIENT';
-
-        // If user has only one role, use it
-        if (roles.length === 1) {
-          defaultRole = roles[0];
-        }
-
-        // If multiple roles → default to CLIENT if exists
-        if (roles.length > 1) {
-          if (roles.includes('CLIENT')) {
-            defaultRole = 'CLIENT';
-          } else {
-            defaultRole = roles[0];
-          }
-        }
-
-        // Set active role
-        this.authService.setActiveRole(defaultRole);
-
-        // Navigate
-        this.navigateByRole(defaultRole);
-
-        this.authService.loading.set(false);
-
-        const pendingProduct = sessionStorage.getItem('pendingCartProduct');
-
-        if (pendingProduct) {
-          this.cartService.addToCart(pendingProduct).subscribe(() => {
-            this.cartService.refreshCartCount();
-            sessionStorage.removeItem('pendingCartProduct');
-          });
-        }
-      },
-      error: () => {
-        this.authService.loading.set(false);
       },
     });
-  }
-
-  private navigateByRole(role: string) {
-    if (role === 'ADMIN') {
-      this.router.navigate(['/admin'], { replaceUrl: true });
-    } else if (role === 'SHOP') {
-      this.router.navigate(['/shop-dashboard'], { replaceUrl: true });
-    } else {
-      this.router.navigate(['/home'], { replaceUrl: true });
-    }
   }
 }
--- src/app/components/main-content/main-content.ts
+++ src/app/components/main-content/main-content.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../shared/services/auth';
 
 @Component({
   selector: 'app-main-content',
@@ -7,4 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
-export class MainContent {}
+export class MainContent {
+  private readonly authService = inject(AuthService);
+  ngOnInit() {
+    if (!localStorage.getItem('token')) {
+      this.authService
+        .login({
+          email: 'all@gmail.com',
+          password: '12345',
+        })
+        .subscribe();
+    }
+  }
+}
