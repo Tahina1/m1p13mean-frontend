1fa5a913f0b5069c757071a7cae2a96472ba9d59
miangaly-rakotoarivelo <miangaly.rakotoarivelo@pulse.mg>
2026-03-04 14:51:35 +0300

    add and edit category

---
+++ src/app/components/main-content/admin-layout/pages/category-list/category-modal/category-modal.ts
@@ -0,0 +1,96 @@
+import { NotificationComponent } from '@/components/shared/components/notification-component/notification-component';
+import { ProductCategory } from '@/components/shared/models/product';
+import { CategoryService } from '@/components/shared/services/category-service';
+import { NotificationService } from '@/components/shared/services/notification-service';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-category-modal',
+  imports: [ReactiveFormsModule, NotificationComponent],
+  templateUrl: './category-modal.html',
+  styleUrl: './category-modal.scss',
+})
+export class CategoryModal {
+  private fb = inject(FormBuilder);
+  private categoryService = inject(CategoryService);
+  private notificationService = inject(NotificationService);
+
+  @Input() isOpen = false;
+  @Input() editCategory: ProductCategory | null = null;
+
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() created = new EventEmitter<void>();
+
+  form = this.fb.group({
+    name: ['', Validators.required],
+    isActive: [true],
+  });
+
+  ngOnChanges() {
+    if (this.editCategory && this.isOpen) {
+      this.form.patchValue({
+        name: this.editCategory.name,
+        isActive: this.editCategory.isActive,
+      });
+    }
+  }
+
+  close() {
+    this.closeModal.emit();
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      this.notificationService.show('Please fill required fields', 'error');
+      return;
+    }
+
+    const v = this.form.value;
+
+    const formData = new FormData();
+    formData.append('name', v.name || '');
+
+    // CREATE
+    if (!this.editCategory) {
+      this.categoryService.createProductCategory(formData).subscribe({
+        next: () => {
+          this.notificationService.show('Category created successfully', 'success');
+          this.created.emit();
+          this.close();
+          this.reset();
+        },
+        error: (err) => {
+          const message = err?.error?.message || 'Failed to create category';
+          this.notificationService.show(message, 'error');
+        },
+      });
+    }
+
+    // UPDATE
+    else {
+      formData.append('isActive', String(v.isActive ?? true));
+
+      this.categoryService.updateProduct(this.editCategory._id, formData).subscribe({
+        next: () => {
+          this.notificationService.show('Category updated successfully', 'success');
+          this.created.emit();
+          this.close();
+          this.reset();
+        },
+        error: (err) => {
+          const message = err?.error?.message || 'Failed to update category';
+          this.notificationService.show(message, 'error');
+        },
+      });
+    }
+  }
+
+  reset() {
+    this.form.reset({
+      name: '',
+      isActive: true,
+    });
+  }
+}
